a01d0c8208bdb96b3f36ab75d6223d2cc18b5f50
timonx11 <TimN@winslow.com.au>
2022-12-08 21:59:36 +1100

    upload files
    Adding folders and all files required for the submission

---
+++ PyBank/main.py
@@ -0,0 +1,94 @@
+# Your task is to create a Python script that analyses the records to calculate each of the following values:
+# The total number of months included in the dataset
+# The net total amount of "Profit/Losses" over the entire period
+# The changes in "Profit/Losses" over the entire period, and then the average of those changes
+# The greatest increase in profits (date and amount) over the entire period
+# The greatest decrease in profits (date and amount) over the entire period
+#-------------------------------------------------------------------------------------------------------------------
+# Your analysis should align with the following results:
+#-------------------------------------------------------------------------------------------------------------------
+# Financial Analysis
+# ----------------------------
+# Total Months: 86
+# Total: $22564198
+
+# Average Change: $-8311.11
+# Greatest Increase in Profits: Aug-16 ($1862002)
+# Greatest Decrease in Profits: Feb-14 ($-1825558)
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#importing Module os and csv
+import os
+import csv
+
+#print header for the analysis result
+print("Financial Analysis")
+print("-------------------------------------------------------------------")
+#defining the path of resources file
+csvfpath = "budget_data.csv"
+
+#Declaring an empty list to hold and separate values from the dictionary from the csv file
+profitloss = []
+months = []
+increase = []
+
+#read through the csv resource file, formating the looks and feel and set delimiter for the dictionary
+with open(csvfpath, "r", newline="",encoding='utf') as csvfile:
+    csvreader = csv.DictReader(csvfile, delimiter=",")
+
+    #for loop to go through the csv file and separate the value columns to the months list and profitloss list
+    for row in csvreader:
+        months.append(row["Date"])
+        profitloss.append(int(row["Profit/Losses"]))
+    
+    #for loop to get the number if iteration to get the average changes 
+    for x in range(len(profitloss)):
+        count = x 
+
+    #use a comprehension to get the difference beetween value and the next value and we also need to zip it so we can iterate it
+    increase = [x[0]-x[1] for x in zip(profitloss[1:],profitloss[:-1])]
+    #add an extra 0 value to the increase list for indexing purpose
+    increase.insert(0, 0)
+
+    #create a new empty dictionary so we can merge the 2 List (increase and months)
+    m = {}
+    #fill up new dictionary
+    for i in range(len(increase)):
+	    m[months[i]] = increase[i]
+
+#get max values of the diference and assign in to max_increase variable    
+max_increase = max(m.values())
+#get the key associated with the max values and assign it to max_key variable
+max_key = max(m, key=m.get)
+#get min values of the diference and assign in to min_increase variable    
+min_increase = min(m.values())
+#get the key associated with the min values and assign it to min_key variable
+min_key = min(m, key=m.get)
+#get the average changes of the profit loss and assign it to avgchanges table
+avgChanges = round(((profitloss[-1] - profitloss[0])/count),2)      
+
+#displaying the rest of analysis results
+print("Total Months:", len(months))
+print(f"Total: ${str(sum(profitloss))}")
+print("")
+print(f"Average Change: ${str(avgChanges)}")
+print(f"Greatest Increase in Profits: {str(max_key)} (${str(max_increase)})")
+print(f"Greatest Decrease in Profits: {str(min_key)} (${str(min_increase)})")           
+
+#exporting the analysis result to txt file
+lines = ['Financial Analysis',
+'-------------------------------------------------------------------',
+'Total Months: 86',
+'Total: $22564198',
+'Average Change: $-8311.11',
+'Greatest Increase in Profits: Aug-16 ($1862002)',
+'Greatest Decrease in Profits: Feb-14 ($-1825558)']
+
+with open('readme.txt', 'w') as f:
+    for line in lines:
+        f.write(line)
+        f.write('\n')
+    
+    
+
+          
+    
---
+++ PyPoll/main.py
@@ -0,0 +1,106 @@
+# In this Challenge, you are tasked with helping a small, rural U.S. town modernise its vote-counting process.
+# You will be given a set of poll data called election_data.csv. The dataset is composed of three columns: "Voter ID", "County", and "Candidate". Your task is to create a Python script that analyzes the votes and calculates each of the following values:
+# The total number of votes cast
+# A complete list of candidates who received votes
+# The percentage of votes each candidate won
+# The total number of votes each candidate won
+# The winner of the election based on popular vote
+# Your analysis should align with the following results:
+
+# Election Results
+# -------------------------
+# Total Votes: 369711
+# -------------------------
+# Charles Casper Stockham: 23.049% (85213)
+# Diana DeGette: 73.812% (272892)
+# Raymon Anthony Doane: 3.139% (11606)
+# -------------------------
+# Winner: Diana DeGette
+# -------------------------
+
+#importing Module os and csv
+import os
+import csv
+#print header for the analysis result
+print("Election Results")
+print("-------------------------------")
+#defining the path of resources file
+csvfpath = "election_data.csv"
+
+#Declaring an empty list to hold and separate values from the csv file
+votes = []
+candidates = []
+Charles = []
+Diana = []
+Raymon = []
+
+#read through the csv resource file, formating the looks and feel and set delimiter for the dictionary
+with open(csvfpath, "r", newline="",encoding='utf') as csvfile:
+    csvreader = csv.DictReader(csvfile, delimiter=",")
+
+    #for loop to go through the csv file and separate the value columns to the months list and profitloss list
+    for row in csvreader:
+        votes.append(row["Ballot ID"])
+        candidates.append(row["Candidate"])
+
+    #for loop to go count the number of votes each candidates get and put it on separate list to extract the results further 
+    for x, candidate in enumerate(candidates):
+        if candidate == 'Raymon Anthony Doane':
+            Raymon.append(candidates[x])
+        elif candidate == 'Charles Casper Stockham':
+            Charles.append(candidates[x])
+        elif candidate == 'Diana DeGette':
+            Diana.append(candidates[x])
+        else:
+            print("Unlisted Candidates")
+
+    #declaring a variable for each candidate to get the votes percentage result    
+    raymon_percent = round(((len(Raymon)/len(votes))*100),3)
+    diana_percent = round(((len(Diana)/len(votes))*100),3)
+    charles_percent = round(((len(Charles)/len(votes))*100),3)
+    
+    #variable needed to compare the results and decide the winner
+    can1 = len(Diana)
+    can2 = len(Raymon)
+    can3 = len(Charles)
+    #creating a function to compare the vote results
+    def max_num(can1, can2, can3):
+        if can1 > can2:
+            if can1 > can3:
+                return (Diana[27])
+            else:
+                return (Charles[0])
+        else:
+            if can2 > can3:
+                return (Raymon[13])
+            else:
+                return (Charles[0])
+
+
+#print the election results
+print("Total Votes:", len(votes))
+print("-------------------------------")
+print(f"Charles Casper Stockham: {str(charles_percent)}% ({len(Charles)})")
+print(f"Diana DeGette: {str(diana_percent)}% ({len(Diana)})")
+print(f"Raymon Anthony Doane: {str(raymon_percent)}% ({len(Raymon)})")
+print("-------------------------------")
+print(f"Winner: {max_num(can1,can2,can3)}")
+print("-------------------------------")
+
+#exporting the election result to txt file
+
+lines = ['Election Results',
+'-------------------------',
+'Total Votes: 369711',
+'-------------------------',
+'Charles Casper Stockham: 23.049% (85213)',
+'Diana DeGette: 73.812% (272892)',
+'Raymon Anthony Doane: 3.139% (11606)',
+'-------------------------',
+'Winner: Diana DeGette',
+'-------------------------']
+
+with open('readmepypoll.txt', 'w') as f:
+    for line in lines:
+        f.write(line)
+        f.write('\n')
